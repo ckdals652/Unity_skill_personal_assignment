0621a5b885677f7c6ed6da8e6ec14d9ac961e42c
ckdals652 <ckdals652@naver.com>
2025-05-22 12:45:56 +0900

    feat : 땅인지 아닌지 판단하는 함수 작성중

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement")]
-    public float MovementSpeed;
+    [Header("Movement")] public float MovementSpeed;
+    public float jumpForce;
+
     public Vector2 currentMovementInput;
-    
+
+    //플레이어 콜라이더 좌측 하단 기준
+    public Transform playerBottomPosition;
+    private Collider playerCollider;
+    public float rayLength = 0.1f;
+    public LayerMask groundLayer;
+    public Ray[] groundRays = new Ray[9];
+
     private Rigidbody playerRigidbody;
+
 
     void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<Collider>();
+    }
+
+    private void Start()
+    {
+        //땅인지 확인할 레이 쏘기
+        GenerateGroundRays();
     }
 
     void FixedUpdate()
@@ -38,20 +54,50 @@ public class PlayerController : MonoBehaviour
 
     public void Move()
     {
-        Vector3 direction = transform.forward*currentMovementInput.y+transform.right*currentMovementInput.x;
+        Vector3 direction = transform.forward * currentMovementInput.y + transform.right * currentMovementInput.x;
         direction *= MovementSpeed;
         direction.y = playerRigidbody.velocity.y;
 
         playerRigidbody.velocity = direction;
     }
 
-    public void OnJump()
+    public void OnJump(InputAction.CallbackContext context)
     {
-        
+        playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
     }
 
-    public void Jump()
+    public void isGrounded()
     {
-        
+        foreach (var ray in groundRays)
+        {
+            
+        }
+    }
+
+    public void GenerateGroundRays()
+    {
+        Bounds bounds = playerCollider.bounds;
+        float width = bounds.size.x;
+        float depth = bounds.size.z;
+
+        int index = 0;
+        int gridCount = 3;
+        float xSpacing = width / (gridCount - 1);
+        float zSpacing = depth / (gridCount - 1);
+
+        Vector3 center = playerBottomPosition.position;
+
+        for (int x = 0; x < gridCount; x++)
+        {
+            for (int z = 0; z < gridCount; z++)
+            {
+                float offsetX = -width / 2f + x * xSpacing;
+                float offsetZ = -depth / 2f + z * zSpacing;
+
+                Vector3 origin = center + new Vector3(offsetX, 0f, offsetZ);
+                groundRays[index] = new Ray(origin, Vector3.down);
+                index++;
+            }
+        }
     }
 }
