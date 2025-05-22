a4b5f90d5cf88ec330440d19791d3db217298e94
ckdals652 <ckdals652@naver.com>
2025-05-22 11:48:26 +0900

    feat : 플레이어 마우스 시점 이동

--- Assets/Scripts/MouseAim.cs
+++ Assets/Scripts/MouseAim.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 
 public class MouseAim : MonoBehaviour
 {
-    [Header("MouseAim")] public Transform playerCameraTransform;
+    [Header("MouseAim")] 
+    public Transform Player;
+    public Transform playerCameraTransform;
     public float minXLook;
     public float maxXLook;
     private float currentXRot;
@@ -21,14 +23,15 @@ public class MouseAim : MonoBehaviour
 
     private void CameraAim()
     {
-        currentXRot += mouseDelta.y * lookSensitivity;
+        currentXRot -= mouseDelta.y * lookSensitivity;
         currentXRot = Mathf.Clamp(currentXRot, minXLook, maxXLook);
+        //카메라만 위아래 각도 돌리기
         playerCameraTransform.localEulerAngles = new Vector3(currentXRot, 0, 0);
-
-        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+        //player객체 전체 좌우 돌리기
+        Player.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
     }
 
-    void OnAim(InputAction.CallbackContext context)
+    public void OnAim(InputAction.CallbackContext context)
     {
         mouseDelta = context.ReadValue<Vector2>();
     }
