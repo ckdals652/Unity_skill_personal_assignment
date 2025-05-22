4ee681f7b8eaaac37bc211b23f1ccc6b8a94e27f
ckdals652 <ckdals652@naver.com>
2025-05-22 15:49:54 +0900

    feat : jumpPad 완성!

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -1,8 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    
+    public float jumpForce;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == (int)Enum.EnumLayer.Player)
+        {
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+        }
+    }
 }
