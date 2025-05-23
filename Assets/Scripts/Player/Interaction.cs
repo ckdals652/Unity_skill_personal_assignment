f1bceb0588dafe4eff7efd5b5b1a09b25c0e9d0c
ckdals652 <ckdals652@naver.com>
2025-05-23 11:10:11 +0900

    Add : ItemInterection 추가

--- Assets/Scripts/Player/Interaction.cs
+++ Assets/Scripts/Player/Interaction.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Interaction : MonoBehaviour
-{
-    // Start is called before the first frame update
+{    
+    public float checkRate = 0.05f;
+    private float lastCheckTime;
+    public float maxCheckDistance;
+    public LayerMask layerMask;
+
+    public GameObject curInteractGameObject;
+    Public ItemInterection; 
+    
+    public TextMeshProUGUI promptText;
+    private Camera camera;
+    
     void Start()
     {
-        
+        camera = Camera.main;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if(hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+    
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = curInteractGameObject.GetComponent<ItemInterection>().GetInteractPrompt();
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameObject = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
     }
 }
