4664ea5df34b0880622bfa495d4e83ae124c3c81
ckdals652 <ckdals652@naver.com>
2025-05-23 09:39:15 +0900

    add : ItemEffectManager 추가

---
+++ Assets/Scripts/ItemEffectManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemEffectManager : MonoBehaviour
+{
+    private static ItemEffectManager _instance;
+
+    public static ItemEffectManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameObject("ItemEffectManager").AddComponent<ItemEffectManager>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            if (_instance != null)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
